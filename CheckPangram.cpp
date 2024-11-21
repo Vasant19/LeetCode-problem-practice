d78ccb4409dac5952b3724862a6380381b01941c
Vasant19 <vasantdave2002@gmail.com>
2024-11-20 19:59:01 -0500

    Added solution for Problem no. 1832,  Check Pangram problem

---
+++ CheckPangram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+class Solution 
+{
+public:
+    bool checkIfPangram(string s) 
+    {
+        unordered_set<char> uniqueChars(s.begin(), s.end());
+        return uniqueChars.size() == 26;
+    }
+};
+
+int main() {
+    Solution s;
+    string sentence = "thequickbrownfoxjumpsoverthelazydog";
+    cout << (s.checkIfPangram(sentence) ? "True" : "False") << endl;
+    string sentence2 = "leetcode";
+    cout << (s.checkIfPangram(sentence2) ? "True" : "False") << endl;
+    string sentence3 = "Thefiveboxingwizardsjumpquickly";
+    cout << (s.checkIfPangram(sentence3) ? "True" : "False") << endl;
+    return 0;
+}
