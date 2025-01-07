1715f864a3504357b7bb8bf31f417a80031c4625
Vasant19 <vasantdave2002@gmail.com>
2025-01-07 17:37:56 -0500

    Implement maxProfit function for Stock Buy and Sell problem and add test cases

--- StockBuyAndSell.cpp
+++ StockBuyAndSell.cpp
@@ -8,12 +8,28 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int ans = 0;
+        int bestbuy = prices[0];
+        for(int i = 1;i<prices.size();i++)
+        {
+            if (prices[i] > bestbuy)
+            {
+                ans = max(ans,prices[i]-bestbuy);
+            }
+            bestbuy = min(bestbuy,prices[i]);
+        }
+        return ans;
     }
 };
 
 
 int main ()
 {
+    Solution s;
+    vector<int> prices = {7,1,5,3,6,4};
+    cout << s.maxProfit(prices) << endl;  // Expected 5
+    vector<int> prices2 = {7,6,4,3,1};
+    cout << s.maxProfit(prices2) << endl; // Expected 0
+    return 0;
 
 }
