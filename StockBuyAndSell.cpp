87b48e6c00ad71cd9dfbe16caeaac35044d902d6
Vasant19 <vasantdave2002@gmail.com>
2025-01-07 17:13:00 -0500

    Add initial structure for Stock Buy and Sell problem no. 121

--- StockBuyAndSell.cpp
+++ StockBuyAndSell.cpp
@@ -1,2 +1,19 @@
 // Problem no. 121 https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 // Meets Linear Runtime Complexity O(n) and Constant Space Complexity requirements
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+    }
+};
+
+
+int main ()
+{
+
+}
