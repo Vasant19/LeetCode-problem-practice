ed7fa0e8e72a5f1ce17e761134c492c13c19f92c
Vasant19 <vasantdave2002@gmail.com>
2025-01-06 20:40:42 -0500

    Add solution for Single Number problem no. 136

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,23 +1,3 @@
 // Problem no. 136 https://leetcode.com/problems/single-number/
 // Meets Linear Runtime Complexity O(n) and Constant Space Complexity requirements 
 // XOR bitwise operator is used to solve this problem 
-#include <iostream> 
-#include <vector>
-using namespace std;
-
-int SingleNumber(vector<int> &nums)
-{
-    int ans = 0;
-    for (int val : nums)
-    {
-        ans ^= val;
-    }
-    return ans;
-}
-
-int main()
-{
-    vector<int> nums = {1, 2, 2, 1, 4,4,5};
-    cout << "Single number is: " << SingleNumber(nums) << endl;
-    return 0;
-}
