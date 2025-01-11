d41c4b60fc87b9673f173754e9b594ecd0227627
Vasant19 <vasantdave2002@gmail.com>
2025-01-11 12:13:08 -0500

    Implement productExceptSelf function and add test cases in main

--- ProductArrayExceptSelf.cpp
+++ ProductArrayExceptSelf.cpp
@@ -8,13 +8,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
+        vector<int> ans;
+
     }
 };
 
 int main()
 {
+    Solution s;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> nums2 = {-1,1,0,-3,3};
+
+    vector<int> result = s.productExceptSelf(nums);
+    for (auto val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
     
+    vector<int> result2 = s.productExceptSelf(nums2);
+    for (auto val : result2) {
+        cout << val << " ";
+    }
     
     return 0;
 }
