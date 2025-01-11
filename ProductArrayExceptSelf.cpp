7923b6b8476b2b1ec35bf49008a59f9dbc43bc32
Vasant19 <vasantdave2002@gmail.com>
2025-01-11 13:48:28 -0500

    Implement productExceptSelf function with nested loops and update output formatting in main

--- ProductArrayExceptSelf.cpp
+++ ProductArrayExceptSelf.cpp
@@ -9,7 +9,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ans;
-
+        int n = nums.size();
+        for(int i = 0;i<n;i++)
+        {
+            int product = 1;
+            for(int j = 0; j < n; j++) {
+                if(i != j) {
+                    product *= nums[j];
+                }
+            }
+            ans.push_back(product);
+        }
+        return ans;
     }
 };
 
@@ -21,14 +32,14 @@ int main()
 
     vector<int> result = s.productExceptSelf(nums);
     for (auto val : result) {
-        cout << val << " ";
+        cout << val << "\n";
     }
     cout << endl;
     
     vector<int> result2 = s.productExceptSelf(nums2);
     for (auto val : result2) {
-        cout << val << " ";
+        cout << val << "\n";
     }
-    
+
     return 0;
 }
