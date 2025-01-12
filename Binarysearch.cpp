728801acbaa04dd772c6c13085ce3bbd5e3c7057
Vasant19 <vasantdave2002@gmail.com>
2025-01-12 18:25:28 -0500

    Implement binary search method in Solution class and update main function for testing

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -7,8 +7,21 @@
 #include <algorithm>
 using namespace std;
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+    }
+};
 
 int main ()
 {
-    
+    Solution s;
+    vector<int> nums = {-1,0,3,5,9,12};
+    int target = 9;
+    int target2 = 2;
+    cout << s.search(nums, target) << endl;
+    cout << s.search(nums, target2) << endl;
+    return 0;
+
 }
