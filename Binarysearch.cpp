5e436b9fde807dcc1815947c519a5d408b7800f7
Vasant19 <vasantdave2002@gmail.com>
2025-01-12 18:05:43 -0500

    Add initial implementation of Binary Search with optimization details

---
+++ Binarysearch.cpp
@@ -0,0 +1,14 @@
+// Problem no. 704 link: https://leetcode.com/problems/binary-search/
+// Optimization technique: Binary search
+// Time complexity: O(log n)
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main ()
+{
+    
+}
