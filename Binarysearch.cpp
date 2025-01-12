c9cd0d5db4354f96bd908ede5a151565b73db883
Vasant19 <vasantdave2002@gmail.com>
2025-01-12 18:48:37 -0500

    Implement binary search algorithm in Solution class

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -10,7 +10,27 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+
+        while (start <= end)
+        {
+            int mid = (start + end)/2;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+            else if (target < nums[mid])
+            {
+                end = mid - 1;
+            }
+            else 
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
     }
 };
 
