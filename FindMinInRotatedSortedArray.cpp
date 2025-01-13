3af108ed35da27acaf2631b678d76744e7c05820
Vasant19 <vasantdave2002@gmail.com>
2025-01-13 14:31:16 -0500

    Refactor findMin method in Solution class for improved logic and efficiency

--- FindMinInRotatedSortedArray.cpp
+++ FindMinInRotatedSortedArray.cpp
@@ -5,43 +5,51 @@
 #include <algorithm>
 using namespace std;
 
-class Solution 
-{
+class Solution {
 public:
-    int findMin(vector<int> &nums) 
-    {
-        int start = 0, n = nums.size(), end = n - 1;
-        int mid = (start + end)/2;
-        int target = INT_MAX;
-        sort(nums.begin(),nums.end());
-        for (auto val : nums)
-        {
-            if(val < target)
-            {
-                target = val;
-                return target;
+    int findMin(vector<int>& nums) {
+        // If there's only one element, return it
+        if (nums.size() == 1) return nums[0];
+        
+        // Set two pointers: left at start and right at end
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        // If array is already sorted (not rotated), return first element
+        if (nums[right] > nums[left]) return nums[0];
+        
+        // Keep searching while left and right don't cross
+        while (left <= right) {
+            // Find middle point
+            int mid = (left + right) / 2;
+            
+            // If middle element is greater than next element,
+            // next element is the minimum
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            
+            // If middle element is less than previous element,
+            // middle element is the minimum
+            if (nums[mid] < nums[mid - 1] ) {
+                return nums[mid];
+            }
+            
+            // If middle element is greater than first element,
+            // minimum is in right half
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            }
+            // If middle element is less than first element,
+            // minimum is in left half
+            else {
+                right = mid - 1;
             }
         }
-        while(start <= end)
-        {
-            if(mid == target)
-            {
-                return mid;
-            }
-            else if(target < mid)
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                start = mid + 1;
-            }
-
-        } 
-    return -1;
+        
+        return -1;
     }
 };
-
 
 
 int main ()
@@ -54,4 +62,4 @@ int main ()
     cout << s.findMin(nums2) << endl;
     cout << s.findMin(nums3) << endl;
     return 0;
-}
+}   
