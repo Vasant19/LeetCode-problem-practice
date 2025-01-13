975cf6052e388ea5ed1c5413ac4e362a3c4a62d7
Vasant19 <vasantdave2002@gmail.com>
2025-01-13 13:27:46 -0500

    Implement findMin method in Solution class and update main function for testing

--- FindMinInRotatedSortedArray.cpp
+++ FindMinInRotatedSortedArray.cpp
@@ -5,9 +5,23 @@
 #include <algorithm>
 using namespace std;
 
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        
+    }
+};
+
 
 
 int main ()
 {
-    
+    Solution s;
+    vector<int> nums = {3,4,5,1,2};
+    vector<int> nums2 = {4,5,6,7,0,1,2};
+    vector<int> nums3 = {11,13,15,17};
+    cout << s.findMin(nums) << endl;
+    cout << s.findMin(nums2) << endl;
+    cout << s.findMin(nums3) << endl;
+    return 0;
 }
