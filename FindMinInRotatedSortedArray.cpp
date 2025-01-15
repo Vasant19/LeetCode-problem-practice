cfe61c325ed39d8662139f6aa6438a85383cef6c
Vasant19 <vasantdave2002@gmail.com>
2025-01-15 12:12:22 -0500

    Improve code formatting for clarity in FindMinInRotatedSortedArray.cpp

--- FindMinInRotatedSortedArray.cpp
+++ FindMinInRotatedSortedArray.cpp
@@ -15,10 +15,13 @@ public:
         int left = 0, right = n - 1;
         while (left < right) {
             int mid = (left + right) /2;
-            if (nums[mid] > nums[right]) {
-            left = mid + 1;
-            } else {
-            right = mid;
+            if (nums[mid] > nums[right]) 
+            {
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid;
             }
         }
         return nums[left];
