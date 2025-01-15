206785db21115bc8b6f53c364ca890fafa419319
Vasant19 <vasantdave2002@gmail.com>
2025-01-15 13:29:12 -0500

    Fix search loop condition and improve readability in SearchRotatedArray.cpp

--- SearchRotatedArray.cpp
+++ SearchRotatedArray.cpp
@@ -12,10 +12,39 @@ public:
     {
         int n = nums.size();
         int left = 0, right = n - 1;
-        while (left < right)
+        while (left <= right)
         {
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) //when mid element is the answer
+            {
+                return mid;
+            }
+
+            if (nums[mid] <= nums[right]) // right sorted
+            {
+                if(nums[mid] <= target && target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else 
+                {
+                    right = mid -1;
+                }
+            } // left sorted
+            else
+            {
+                if(nums[mid] >= target && target >= nums[left])
+                {
+                    right = mid - 1;
+                }
+                else 
+                {
+                    left = mid + 1;
+                }
+            }
         }
+    return -1;
     }
 };
    
@@ -26,8 +55,8 @@ int main ()
     vector<int> nums = {4,5,6,7,0,1,2};
     vector<int> nums2 = {1};
     int target = 0,target2 = 3;
-    cout << s.search(nums,target) << endl;
-    cout << s.search(nums,target2) << endl;
-    cout << s.search(nums2,target) << endl;
+    cout << s.search(nums,target) << endl; // Expected output is 4
+    cout << s.search(nums,target2) << endl; // Expected output is -1
+    cout << s.search(nums2,target) << endl; // Expected output is -1
     return 0;
 }
