2f73039e3dedb4c3245cf03c9163ac6b72af99de
Vasant19 <vasantdave2002@gmail.com>
2025-01-16 21:43:59 -0500

    Rename PeakIndexMountainArray to PeakIndexMountainArray.cpp and implement peakIndexInMountainArray function

--- PeakIndexMountainArray.cpp
+++ PeakIndexMountainArray.cpp
@@ -10,7 +10,15 @@ using namespace std;
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        
+        int ans = 0;
+        for (int i = 0;i<arr.size();i++)
+        {
+            if (arr[i] > arr[ans])
+            {
+                ans = i;
+            }
+        }
+    return ans;
     }
 };
 
