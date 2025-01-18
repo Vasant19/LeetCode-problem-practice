9805db5f26ad45be6b08265541f43c2c9508a080
Vasant19 <vasantdave2002@gmail.com>
2025-01-18 17:57:23 -0500

    Implement binary search algorithm in singleNonDuplicate method for efficient unique element identification

--- SingleNumberSortedArray.cpp
+++ SingleNumberSortedArray.cpp
@@ -12,7 +12,25 @@ class Solution
 public:
     int singleNonDuplicate(vector<int>& nums) 
     {
-        
+        int n = nums.size();
+        int start = 0, end = n - 1;
+        while (start <= end)
+        {
+            int mid = start + (end - start) /2;
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+            {
+                return mid;
+            }
+            else if(nums[mid] == nums[mid-1] && (mid % 2) == 0)
+            {
+                end = mid - 1;
+            }
+            else 
+            {
+                start = mid + 1;
+            }
+        }
+    return -1;
     }
 };
 
