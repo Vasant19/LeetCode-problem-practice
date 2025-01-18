ee981267bd9ed6cce71b2cd82c4576cde58c73a4
Vasant19 <vasantdave2002@gmail.com>
2025-01-18 17:01:14 -0500

    Add main function and include necessary headers in SingleNumberSortedArray.cpp

--- SingleNumberSortedArray.cpp
+++ SingleNumberSortedArray.cpp
@@ -1,3 +1,14 @@
 // Problem no 540 link: https://leetcode.com/problems/single-element-in-a-sorted-array/
 // O(logN) solution
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main()
+{
+    return 0;
+}
