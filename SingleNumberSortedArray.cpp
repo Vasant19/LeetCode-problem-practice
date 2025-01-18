c8a5fa8a0c03c37ac7c86e0449cca6e278518973
Vasant19 <vasantdave2002@gmail.com>
2025-01-18 17:20:13 -0500

    Implement singleNonDuplicate method using set for unique element identification in SingleNumberSortedArray.cpp

--- SingleNumberSortedArray.cpp
+++ SingleNumberSortedArray.cpp
@@ -4,16 +4,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int ans = 0;
+        set<int> result;
+        int n = nums.size();
+        result.insert(nums.begin(), nums.end());
+        for(int i = 0;i<n;i++)
+        {
+            auto vals = result.find(nums[i]);
+            
+
+        }
+    return ans;
     }
 };
 
 int main()
 {
+    Solution s;
+    vector<int>nums = {1,1,2,3,3,4,4,8,8};
+    vector<int>nums2 = {3,3,7,7,10,11,11};
+    cout << s.singleNonDuplicate(nums) << "\n";
+    cout << s.singleNonDuplicate(nums2) << "\n";
     return 0;
 }
