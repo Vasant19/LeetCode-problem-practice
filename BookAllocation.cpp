dc540c2258a5731189e6150fdb7241c0134e0f1a
Vasant19 <vasantdave2002@gmail.com>
2025-01-19 19:34:46 -0500

    Add allocateBooks function prototype and sample input in BookAllocation.cpp

--- BookAllocation.cpp
+++ BookAllocation.cpp
@@ -7,8 +7,15 @@
 #include <set>
 using namespace std;
 
+int allocateBooks(vector<int> &arr, int n, int m)
+{
 
+}
 int main()
 {
+    vector<int> arr = {2,1,3,4};
+    int n = 4, m = 2;
+    
+    cout << allocateBooks(arr, n, m) << endl; 
     return 0;
 }
