7fc9b68da123831cf1850eb1344761a68ea352ba
Vasant19 <vasantdave2002@gmail.com>
2025-01-19 19:12:41 -0500

    Add initial implementation of BookAllocation problem

---
+++ BookAllocation.cpp
@@ -0,0 +1,14 @@
+// link https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+int main()
+{
+    return 0;
+}
