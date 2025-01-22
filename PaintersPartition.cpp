ca1c277cfbfedba7f728fdef401e5380c07c5878
Vasant19 <vasantdave2002@gmail.com>
2025-01-22 14:02:55 -0500

    Add initial implementation of Painters Partition problem with binary search setup

---
+++ PaintersPartition.cpp
@@ -0,0 +1,18 @@
+// link: https://www.geeksforgeeks.org/painters-partition-problem/
+// Variant of book allocation problem
+// Binary search
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+
+
+int main()
+{
+    return 0;
+}
