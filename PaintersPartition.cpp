b91139723a144ccb144b7f1d765d2aac3aa4ab2c
Vasant19 <vasantdave2002@gmail.com>
2025-01-22 14:12:47 -0500

    Add input validation for ParititionsPainted function in PaintersPartition.cpp

--- PaintersPartition.cpp
+++ PaintersPartition.cpp
@@ -1,22 +1,27 @@
 // link: https://www.geeksforgeeks.org/painters-partition-problem/
 // Variant of book allocation problem
 // Binary search
-
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <set>
 using namespace std;
 
-
+// N boards, M painters 
 int ParititionsPainted(vector<int> &arr,int n,int m)
 {
+    if (m > n)
+    {
+        return -1;
+    }
+    int ans = -1;
 
 }
 
 int main()
 {
     vector<int> arr = {40,30,10,20};
+    int n = 4,m = 2;
+    cout << ParititionsPainted(arr,n,m) << endl;
     return 0;
 }
