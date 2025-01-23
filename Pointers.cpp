b968906ae46801eecfa6088833d1290bcdfbae20
Vasant19 <vasantdave2002@gmail.com>
2025-01-23 18:43:37 -0500

    Enhance Pointers.cpp with examples of pointer and pointer-to-pointer usage

--- Pointers.cpp
+++ Pointers.cpp
@@ -7,9 +7,16 @@
 #include <algorithm>
 using namespace std;
 
-
-
 int main()
 {
+    int x = 5;
+    int* ptr = &x; // ptr is a pointer to an integer
+    int** ptr2 = &ptr; // ptr2 is a pointer to a pointer to an integer
+    cout << "The value of x is: " << x << endl;
+    cout << "The address of x is: " << &x << endl;
+    cout << "The value of x is: " << *ptr << endl; // Dereferencing the pointer
+    cout << "The address of ptr is: " << &ptr << endl;
+    cout << "The value of x is: " << **ptr2 << endl; // Dereferencing the pointer to a pointer
+    cout << "The address of ptr2 is: " << &ptr2 << endl;
     return 0;
 }
