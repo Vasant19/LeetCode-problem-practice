f2fbdde4dd5f4a16513c75d3f7378e5bb0ee3fab
Vasant19 <vasantdave2002@gmail.com>
2025-01-24 14:40:58 -0500

    Add foundational code for Insertion Sort algorithm in InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,14 @@
+// Foundational code on Insertion sort
+// Insertion sort is an algorithm that builds the final sorted array one item at a time.
+// Time complexity: O(n^2)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    
+    return 0;
+}
