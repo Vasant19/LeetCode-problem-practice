a514d3e802d1702ddd1b5c661b165f9bdd46305f
Vasant19 <vasantdave2002@gmail.com>
2025-01-24 18:11:15 -0500

    Implement Insertion Sort algorithm and add array printing function in InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -6,9 +6,35 @@
 #include <algorithm>
 using namespace std;
 
+void InsertionSort(int arr[],int n)
+{
+    for (int i = 1;i<n;i++)
+    {
+        int current = i;
+        int prev = i-1;
+        while (arr[prev] > current && prev >= 0)
+        {
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = current;
+    }
+}
+
+void printarray(int arr[],int n)
+{
+    for (int i = 0;i<n;i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
 
 int main()
 {
-
+    int n = 5;
+    int arr[]= {4,1,5,2,3};
+    InsertionSort(arr,n);        
+    cout << "Sorted array: \n";
+    printarray(arr,n);
     return 0;
 }
