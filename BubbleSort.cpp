1f87c63935220ca133ae8f128862739bfcf0e4bb
Vasant19 <vasantdave2002@gmail.com>
2025-01-23 19:39:37 -0500

    Implement Bubble Sort algorithm and add array printing function in BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -7,7 +7,39 @@
 #include <algorithm>
 using namespace std;
 
+void bubblesort(int arr[],int n)
+{
+    // TC = O(n^2)
+    for (int i = 0;i<n-1;i++)
+    {
+        bool isSwap = false;
+        for (int j = 0;j < n-i-1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+                isSwap = true;
+            }
+        }
+        if (!isSwap)
+        {
+            return;
+        }
+    }
+}
+
+void printarray(int arr[],int n)
+{
+    for (int i = 0;i<n-1;i++)
+    {
+        cout << arr[i] << "\n"
+    }
+    cout << endl;
+}
+
 int main()
 {
+    int n = 5;
+    vector<int> arr = {4,1,5,2,3};
     return 0;
 }
