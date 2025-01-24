fa9c2e88f266053ce3d057011db0c913620c6eda
Vasant19 <vasantdave2002@gmail.com>
2025-01-24 14:35:19 -0500

    Add main function to SelectionSort.cpp and include necessary headers

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,3 +1,20 @@
 // Foundational code on Selection Sort algorithm
 // Selection sort is an algorithm that selects the smallest element from an unsorted list in each iteration and places it at the beginning of the list.
 // Time complexity: O(n^2)
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+
+int main()
+{
+    int n = 5;
+    int arr[]= {4,1,5,2,3};
+    bubblesort(arr,n);
+    cout << "Sorted array: \n";
+    printarray(arr,n);
+    return 0;
+}
