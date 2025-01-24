c440aac68343ceab27a847b072e46770cef028bc
Vasant19 <vasantdave2002@gmail.com>
2025-01-24 14:38:00 -0500

    Implement Selection Sort algorithm and add array printing function in SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -6,14 +6,36 @@
 #include <algorithm>
 using namespace std;
 
+void Selectionsort(int arr[],int n)
+{
+    for (int i = 0;i<n-1;i++)
+    {
+        int smallestindex = i;
+        for (int j = i+1;j<n;j++)
+        {
+            if (arr[j] < arr[smallestindex])
+            {
+                smallestindex = j;
+            }
+        }
+        swap(arr[i],arr[smallestindex]);
 
+    }
+}
 
+void printarray(int arr[],int n)
+{
+    for (int i = 0;i<n;i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
 
 int main()
 {
     int n = 5;
     int arr[]= {4,1,5,2,3};
-    bubblesort(arr,n);
+    Selectionsort(arr,n);
     cout << "Sorted array: \n";
     printarray(arr,n);
     return 0;
