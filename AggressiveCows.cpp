ed3a35ae15b277ad00370f6834297b9a0095ad79
vasant <vasantdave2002@gmail.com>
2025-01-28 07:03:15 -0500

    Add time complexity comments and additional test case for getDistance function in Aggressive Cows implementation

--- AggressiveCows.cpp
+++ AggressiveCows.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 using namespace std;
 
-bool isPossible(vector<int> &stalls, int k, int minimumAllowedDistance)
+bool isPossible(vector<int> &stalls, int k, int minimumAllowedDistance) // O(n)
 {
     int cows = 1, last = stalls[0];
     for (int i = 1; i < stalls.size(); i++)
@@ -27,12 +27,12 @@ bool isPossible(vector<int> &stalls, int k, int minimumAllowedDistance)
 
 int getDistance(vector<int> &stalls, int k)
 {
-    sort(stalls.begin(), stalls.end());
+    sort(stalls.begin(), stalls.end()); // O(nlogn)
     int n = stalls.size();
 
     int st = 1, end = stalls[n-1]-stalls[0], ans = -1;
 
-    while (st <= end)
+    while (st <= end) // O(log(Range) * N)
     {
         int mid = st + (end - st) / 2;
         if(isPossible(stalls, k, mid))
@@ -45,13 +45,14 @@ int getDistance(vector<int> &stalls, int k)
             end = mid - 1;
         }
     }
-
+    return ans;
 }
 int main()
 {
-    vector<int> stalls = {1, 2, 4, 8, 9};
-    vector<int> stalls2 = {10, 1, 2, 7, 5};
+    vector<int> stalls = {1, 2, 4, 8, 9}; // Output: 3
+    vector<int> stalls2 = {10, 1, 2, 7, 5}; // Output: 4
     int k = 3;
     cout << getDistance(stalls, k) << endl;
+    cout << getDistance(stalls2, k) << endl;
     return 0;
 }
