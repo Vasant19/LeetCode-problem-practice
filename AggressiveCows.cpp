0f59937e390b78ac82a6bdc02173b15931edce0a
vasant <vasantdave2002@gmail.com>
2025-01-28 06:15:53 -0500

    Add main function and sample data for Aggressive Cows problem implementation

--- AggressiveCows.cpp
+++ AggressiveCows.cpp
@@ -1,3 +1,14 @@
 // Problem link: https://www.geeksforgeeks.org/problems/aggressive-cows/0, https://www.spoj.com/problems/AGGRCOW/
 // Binary search problem
 // Time Complexity: O(nlogn)
+
+
+
+int main()
+{
+    int stalls[] = {1, 2, 4, 8, 9};
+    int stalls2[] = {10, 1, 2, 7, 5};
+    int k = 3;
+    
+    return 0;
+}
