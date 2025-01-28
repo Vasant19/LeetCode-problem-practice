966bcff76a5e0042bacf68f991c748ca915b25d0
vasant <vasantdave2002@gmail.com>
2025-01-28 06:37:43 -0500

    Add function prototype for getDistance and update stalls arrays to use vectors in Aggressive Cows implementation

--- AggressiveCows.cpp
+++ AggressiveCows.cpp
@@ -2,12 +2,20 @@
 // Binary search problem
 // Time Complexity: O(nlogn)
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
 
+int getDistance(vector<int> &stalls, int k)
+{
+
+}
 int main()
 {
-    int stalls[] = {1, 2, 4, 8, 9};
-    int stalls2[] = {10, 1, 2, 7, 5};
+    vector<int> stalls = {1, 2, 4, 8, 9};
+    vector<int> stalls2 = {10, 1, 2, 7, 5};
     int k = 3;
     
     return 0;
