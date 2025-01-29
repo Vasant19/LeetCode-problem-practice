758614f243ea9c210ed742f24ee4c6dda3e83432
Vasant19 <vasantdave2002@gmail.com>
2025-01-29 12:42:02 -0500

    Implement sortColors function and add test cases in main for Sort Colors problem

--- SortColors.cpp
+++ SortColors.cpp
@@ -4,11 +4,29 @@
 #include <algorithm>
 using namespace std;
 
-
-
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+    }
+};
 
 int main()
 {
+    Solution s;
+    vector<int> nums = {2,0,2,1,1,0};
+    vector<int> nums2 = {2,0,1};
+    s.sortColors(nums);
+    s.sortColors(nums2);
+    for (int i = 0; i < nums.size(); i++)
+    {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < nums2.size(); i++)
+    {
+        cout << nums2[i] << " ";
+    }
     return 0;
 }
 
