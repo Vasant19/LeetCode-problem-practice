a85aed0cc738d9714fc6c50d83f7889125185f0e
vasant <vasantdave2002@gmail.com>
2025-01-31 18:15:16 -0500

    Implement merge function for merging two sorted arrays in-place

--- MergeSortedArray.cpp
+++ MergeSortedArray.cpp
@@ -8,7 +8,22 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
+        int idx = m+n-1,i = m-1, j = n-1;
+        while (i >= 0 && j >= 0)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                nums1[idx] = nums1[i];
+                i--;
+                idx--;
+            }
+            else
+            {
+                nums1[idx] = nums2[j];
+                j--;
+                idx--;
+            }
+        }
     }
 };
 
