91aa4ec118c0f92f11e40e5e0d05c217aec02cdc
vasant <vasantdave2002@gmail.com>
2025-01-31 18:38:45 -0500

    Implement skeleton for nextPermutation function in NextPermutation.cpp

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -1,6 +1,14 @@
 // Problem no. 31 link: https://leetcode.com/problems/next-permutation/
 
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+    }
+};
 
 int main()
 {
