0d34986dfe54c94dfebe717dbcdb772744949edf
vasant <vasantdave2002@gmail.com>
2025-02-02 20:01:55 -0500

    Add description for pivot finding in NextPermutation.cpp

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -1,5 +1,6 @@
 // Problem no. 31 link: https://leetcode.com/problems/next-permutation/
 // Time complexity: O(n), Space complexity: O(1)
+// Find the pivot (special element) which is the first element that is smaller than the element after it
 #include <bits/stdc++.h>
 using namespace std;
 
