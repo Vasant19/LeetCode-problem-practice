e616d1e90fdc653f030505ed34aa1f79207b3838
Vasant <vasantdave2002@gmail.com>
2025-05-15 11:59:13 -0400

    added Linked list common algorithms and starting template for prob no. 18 LC

---
+++ RemoveNthNode.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        
+    
