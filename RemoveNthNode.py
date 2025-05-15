b27e4c98ae36963b5844b611ff5b99df1c7a3112
Vasant <vasantdave2002@gmail.com>
2025-05-15 12:07:53 -0400

    Modified doubly linked list

--- RemoveNthNode.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        
-    
